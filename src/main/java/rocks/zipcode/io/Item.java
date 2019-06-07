a68b1776396272fd861ee38210a78f43b0e88fb4
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-06-07 09:58:16 -0400

    First Commit

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -1,33 +1,48 @@
 package rocks.zipcode.io;
 
+import com.sun.tools.javac.util.Convert;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class Item {
+public class Item implements Comparable<Item>{
+    Long id = 0L;
+    String name = "";
+    Double price = 0.01;
+
     public Item() {
     }
 
     public Item(Long id, String name, Double price) {
+        this.id=id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) {this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id) {this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(Double price) {this.price=price;
+    }
+
+    public int compareTo(Item o) {
+        if(this.id > o.id) return 1;
+        if(this.id < o.id) return -1;
+        else return 0;
     }
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,37 @@
 package rocks.zipcode.io;
 
+
 import java.util.Comparator;
+
+
+
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] myList;
     public ItemSorter(Item[] items) {
+        myList = items;
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        for(int i = 0; i< myList.length;i++){
+            for( int j = i; j>0; j--){
+                if(myList[j].compareTo(myList[j-1])<0) {
+                    Item temp = myList[j];
+                    myList[j] = myList[j-1];
+                    myList[j-1] = temp;
+
+
+
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return myList;
     }
 }
